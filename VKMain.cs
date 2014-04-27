04c4e39937d4d4901a1f5df2aa8a19245eaf08c3
CAWCAWCAW <specialops0@hotmail.com>
2014-04-26 21:32:37 -0400

    V 0.7 Beta
    - Re-coded the timer to make it better
    - Fixed the timer, and conditions so it kicks properly

---
+++ VKMain.cs
@@ -0,0 +1,253 @@
+﻿﻿using System;
+using System.IO;
+using System.Linq;
+using System.ComponentModel;
+using System.Collections.Generic;
+using TShockAPI;
+using TShockAPI.Extensions;
+using Terraria;
+using Newtonsoft.Json;
+using TerrariaApi;
+using TerrariaApi.Server;
+using TShockAPI.DB;
+
+namespace VoteKick
+{
+    [ApiVersion(1, 15)]
+    public class Votekick : TerrariaPlugin
+    {
+        private static VoteKickTimer _timers;
+
+        public override Version Version
+        {
+            get { return new Version("0.7"); }
+        }
+
+        public override string Name
+        {
+            get { return "Votekick"; }
+        }
+
+        public override string Author
+        {
+            get { return "CAWCAWCAW"; }
+        }
+
+        public override string Description
+        {
+            get { return "Vote to kick players"; }
+        }
+
+        public Votekick(Main game)
+            : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            ReadConfig();
+            _timers = new VoteKickTimer();
+            _timers.Start();
+
+            TShockAPI.Commands.ChatCommands.Add(new Command("caw.votekick", Vote, "votekick", "vk"));
+            TShockAPI.Commands.ChatCommands.Add(new Command("caw.reloadvotekick", Reload_Config, "reloadvotekick", "rvk"));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+            }
+            base.Dispose(disposing);
+        }
+        public static bool VoteKickRunning = false;
+        public static Poll poll = new Poll();
+        
+        public class Poll
+        {
+            public List<TSPlayer> voters;
+            public List<TSPlayer> votedyes;
+            public List<TSPlayer> votedno;
+            public TSPlayer playertobekicked;
+            public Poll()
+            {
+                voters = new List<TSPlayer>();
+                votedyes = new List<TSPlayer>();
+                votedno = new List<TSPlayer>();
+            }
+        }
+
+        public static void Vote(CommandArgs args)
+        {
+            if (args.Parameters.Count == 0)
+            {
+                args.Player.SendErrorMessage("Invalid syntax! Proper syntax: /votekick [kick/voteyes/voteno/info/cancel]");
+                return;
+            }
+            switch (args.Parameters[0])
+            {
+                case "voteyes":
+                    if (!poll.voters.Contains(args.Player) && VoteKickRunning)
+                    {
+                        args.Player.SendSuccessMessage("You have voted yes in the vote kick.");
+                        poll.voters.Add(args.Player);
+                        poll.votedyes.Add(args.Player);
+                    }
+                    else if (poll.voters.Contains(args.Player))
+                    {
+                        args.Player.SendErrorMessage("You have already voted for this vote!");
+                    }
+                    else
+                    {
+                        args.Player.SendInfoMessage("A vote is not running at this time.");
+                    }
+                    break;
+                case "voteno":
+                    if (!poll.voters.Contains(args.Player) && VoteKickRunning)
+                    {
+                        args.Player.SendSuccessMessage("You have voted no in the vote kick.");
+                        poll.voters.Add(args.Player);
+                        poll.votedno.Add(args.Player);
+                    }
+                    else if (poll.voters.Contains(args.Player))
+                    {
+                        args.Player.SendErrorMessage("You have already voted for this vote!");
+                    }
+                    else
+                    {
+                        args.Player.SendInfoMessage("A vote is not running at this time.");
+                    }
+                    break;
+
+                case "kick":
+                    if (args.Parameters.Count > 1 && !VoteKickRunning)
+                    {
+                        string playerstring = String.Join(" ", args.Parameters.GetRange(1, args.Parameters.Count - 1));
+                        var players = TShock.Utils.FindPlayer(playerstring);
+                        var plyr = players[0];
+                        if (players.Count == 0)
+                        {
+                            args.Player.SendErrorMessage("No player matched your query '{0}'", playerstring);
+                        }
+                        else if (players.Count > 1)
+                        {
+                            TShock.Utils.SendMultipleMatchError(args.Player, players.Select(p => p.Name));
+                        }
+
+                        TSPlayer.All.SendWarningMessage(args.Player.Name + " has started a votekick against " + plyr.Name);
+                        VoteKickRunning = true;
+                        poll.playertobekicked = plyr;
+                    }
+                    else if (VoteKickRunning)
+                    {
+                        args.Player.SendErrorMessage("A player has already started a votekick on " + poll.playertobekicked.Name);
+                    }
+                    else
+                    {
+                        args.Player.SendErrorMessage("Error! Please use /votekick kick playername");
+                    }
+                    break;
+
+                case "info":
+                    if (VoteKickRunning)
+                        args.Player.SendInfoMessage("Total Players: {0} Player to be kicked: {1}, Votes Yes: {2}, Votes No: {3}", TShock.Utils.ActivePlayers(), poll.playertobekicked.Name, poll.votedyes.Count, poll.votedno.Count);
+                    else
+                        args.Player.SendErrorMessage("There is no votekick running at this time");
+                    break;
+
+                case "cancel":
+                    if (args.Player.Group.HasPermission("caw.cancelvotekick"))
+                    {
+                        poll.voters.Clear();
+                        poll.votedno.Clear();
+                        poll.votedyes.Clear();
+                        poll.playertobekicked = null;
+                        VoteKickRunning = false;
+
+                        TSPlayer.All.SendInfoMessage(args.Player.Name + " has canceled the votekick to kick: " + poll.playertobekicked.Name);
+                    }
+                    else
+                    {
+                        args.Player.SendErrorMessage("You do not have permission to use that command!");
+                    }
+                    break;
+            }
+        }
+
+        public static Config config;
+        private static void CreateConfig()
+        {
+            string filepath = Path.Combine(TShock.SavePath, "Votekick.json");
+            try
+            {
+                using (var stream = new FileStream(filepath, FileMode.Create, FileAccess.Write, FileShare.Write))
+                {
+                    using (var sr = new StreamWriter(stream))
+                    {
+                        config = new Config();
+                        var configString = JsonConvert.SerializeObject(config, Formatting.Indented);
+                        sr.Write(configString);
+                    }
+                    stream.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.ConsoleError(ex.Message);
+            }
+        }
+
+        private static bool ReadConfig()
+        {
+            string filepath = Path.Combine(TShock.SavePath, "Votekick.json");
+            try
+            {
+                if (File.Exists(filepath))
+                {
+                    using (var stream = new FileStream(filepath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                    {
+                        using (var sr = new StreamReader(stream))
+                        {
+                            var configString = sr.ReadToEnd();
+                            config = JsonConvert.DeserializeObject<Config>(configString);
+                        }
+                        stream.Close();
+                    }
+                    return true;
+                }
+                else
+                {
+                    Log.ConsoleError("Votekick config not found. Creating new one...");
+                    CreateConfig();
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.ConsoleError(ex.Message);
+            }
+            return false;
+        }
+
+        public class Config
+        {
+            public double PercentofPlayersVoteYesToKick = 75;
+            public string seconds = "Vote time in seconds";
+            public double VoteTime = 10;
+            public string KickMessage = "You have been vote kicked from the server";
+
+        }
+
+        private void Reload_Config(CommandArgs args)
+        {
+            if (ReadConfig())
+            {
+                args.Player.SendMessage("Votekick config reloaded sucessfully.", Color.Yellow);
+            }
+            else
+            {
+                args.Player.SendErrorMessage("Votekick config reloaded unsucessfully. Check logs for details.");
+            }
+        }
+    }
+}
---
+++ VKTimer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using System.Timers;
+
+namespace VoteKick
+{
+    public class VoteKickTimer
+    {
+        private Timer VKTimer;
+        private Timer VKTimerNotify;
+
+        public VoteKickTimer()
+        {
+            VKTimer = new Timer(Votekick.config.VoteTime * 1000);
+            VKTimerNotify = new Timer((Votekick.config.VoteTime / 2) * 1000);
+        }
+
+        public void Start()
+        {
+            VKTimer.Enabled = true;
+            VKTimer.Elapsed += UpdateTimer;
+
+            VKTimerNotify.Enabled = true;
+            VKTimerNotify.Elapsed += NotifyTimer;
+        }
+
+        private void UpdateTimer(object sender, ElapsedEventArgs args)
+        {
+            if (Votekick.VoteKickRunning)
+            {
+                double active = TShock.Utils.ActivePlayers();
+                double percentageofactive = ((active) * (Votekick.config.PercentofPlayersVoteYesToKick / 100));
+                double totalvoters = Votekick.poll.voters.Count;
+
+                if (Votekick.poll.votedyes.Count > Votekick.poll.votedno.Count && Votekick.poll.votedyes.Count >= percentageofactive)
+                {
+                    TShock.Utils.Kick(Votekick.poll.playertobekicked, Votekick.config.KickMessage, true, false);
+                    Votekick.VoteKickRunning = false;
+                    Votekick.poll.voters.Clear();
+                    Votekick.poll.votedno.Clear();
+                    Votekick.poll.votedyes.Clear();
+                    Votekick.poll.playertobekicked = null;
+                }
+
+                else
+                {
+                    TSPlayer.All.SendInfoMessage("The votekick on " + Votekick.poll.playertobekicked.Name + " has failed.");
+                    Votekick.VoteKickRunning = false;
+                    Votekick.poll.voters.Clear();
+                    Votekick.poll.votedno.Clear();
+                    Votekick.poll.votedyes.Clear();
+                    Votekick.poll.playertobekicked = null;
+                }
+            }
+        }
+        private void NotifyTimer(object sender, ElapsedEventArgs args)
+        {
+            if (Votekick.VoteKickRunning)
+            {
+                TSPlayer.All.SendSuccessMessage("Votekick ending in {0} seconds to kick {1} ", (Votekick.config.VoteTime / 2), Votekick.poll.playertobekicked.Name);
+            }
+        }
+    }
+}
